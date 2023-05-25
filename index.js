4cf69cf0e616c3fe155aff872d7cf939ece95aa0
clarine <antibaclarine.com>
2023-05-25 20:24:53 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.getElementById('body').style.backgroundColor='silver'
+let li= document.createElement('li');
+document.getElementById('li').appendChild(li)
+li.innerHTML='pineapple'
+document.getElementById('title').style.color='green'
+let h3= document.createElement('h3');
+document.getElementById('h3').textContent.toUpperCase()
+let l= document.createElement('l');
+document.getElementById('l').appendChild(l)
+l.innerHTML='cabbage'
